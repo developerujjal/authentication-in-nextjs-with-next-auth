5b3a9c4fa143afc01b9bc5ad24d806603a2c1bae
developerujjal <ujjal.vision02@gmail.com>
2025-02-24 02:29:30 +0600

    usderstand POST request

---
+++ src/app/api/comments/route.jsx
@@ -0,0 +1,32 @@
+
+
+export async function POST(request) {
+    const res = await request.json();
+    comments.push({
+        id: res?.id,
+        des: res?.des
+    })
+
+    return Response.json({
+        message: 'successfully',
+        comments
+    })
+
+}
+
+
+
+const comments = [
+    {
+        id: 1,
+        des: 'hwllo there'
+    },
+    {
+        id: 2,
+        des: 'hwllo there'
+    },
+    {
+        id: 3,
+        des: 'hwllo there'
+    }
+]
