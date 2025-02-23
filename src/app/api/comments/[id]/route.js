88e7ef07237ae451e828b3e25531b125e35e7395
developerujjal <ujjal.vision02@gmail.com>
2025-02-24 02:50:38 +0600

    usderstand PATCH mathod

---
+++ src/app/api/comments/[id]/route.js
@@ -0,0 +1,42 @@
+export async function GET() {
+    return Response.json({
+        comments
+    })
+}
+
+
+export async function PATCH(request, { params }) {
+    const body = await request.json();
+    console.log(body)
+    const { id } = await params;
+    console.log(id)
+
+    const index = comments.findIndex(c => c.id === parseInt(id));
+
+    comments[index] = {
+        id: id,
+        des: body
+    }
+
+    return Response.json({
+        comments
+    })
+
+}
+
+
+
+const comments = [
+    {
+        id: 1,
+        des: 'hwllo there'
+    },
+    {
+        id: 2,
+        des: 'hwllo there'
+    },
+    {
+        id: 3,
+        des: 'hwllo there'
+    }
+]
