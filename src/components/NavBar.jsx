49c7c5423f54d1879d1bf6bf027a2155b72f341c
developerujjal <ujjal.vision02@gmail.com>
2025-02-24 21:50:52 +0600

    recap the api handlers

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,6 +6,7 @@ const NavBar = () => {
         <ul className='flex justify-center gap-5 py-2 bg-yellow-400'>
             <li><Link href={'/'}>Home</Link></li>
             <li><Link href={'/about'}>About</Link></li>
+            <li><Link href={'/posts'}>Posts</Link></li>
             <li><Link href={'/service'}>Service</Link></li>
         </ul>
     );
